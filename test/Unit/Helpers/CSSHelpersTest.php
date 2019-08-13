76f086b11e64abd1f6737b429ca6eb17cb0b6981
ELVNS <6993449@qq.com>
2019-08-14 04:19:37 +0800

    Create CSSHelpersTest.php

---
+++ test/Unit/Helpers/CSSHelpersTest.php
@@ -0,0 +1,14 @@
+<?php
+namespace Tests\Unit\Helpers;
+use Src\Helpers\CSSHelpers;
+use Tests\BaseTestCase;
+class CSSHelpersTest extends BaseTestCase
+{
+    public function testFormatString()
+    {
+        $this->assertEquals("'foo\\'bar'", CSSHelpers::formatString("foo'bar"));
+        $this->assertEquals("'foo\\\\bar'", CSSHelpers::formatString("foo\\bar"));
+        $this->assertEquals("'foo\\\nbar'", CSSHelpers::formatString("foo\nbar"));
+        $this->assertEquals("'foo\\'bar\\\\baq\\\nbaz'", CSSHelpers::formatString("foo'bar\\baq\nbaz"));
+    }
+}
