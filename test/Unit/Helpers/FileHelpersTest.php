4c80efa77b8b212cba7885edb975896c1f0d1c9b
ELVNS <6993449@qq.com>
2019-08-14 04:20:14 +0800

    Create FileHelpersTest.php

---
+++ test/Unit/Helpers/FileHelpersTest.php
@@ -0,0 +1,16 @@
+<?php
+namespace Tests\Unit\Helpers;
+use Src\Helpers\FileHelpers;
+use Tests\BaseTestCase;
+class FileHelpersTest extends BaseTestCase
+{
+    public function testConcatPath()
+    {
+        $this->assertEquals('../dir/subdir/foo/bar', FileHelpers::concatPath('../dir/subdir/', '/foo', null, 'bar/'));
+        $this->assertEquals('http://google.com/search', FileHelpers::concatPath('http://google.com/', '/search', null));
+        $this->assertEquals('C:/windows\\system32', FileHelpers::concatPath(null, 'C:\\', '\\windows\\system32'));
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage('Argument 1 expected to be string or null, double given.');
+        FileHelpers::concatPath('../fir/subdir/', 1.5, '/bar');
+    }
+}
